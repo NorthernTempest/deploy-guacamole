bcb81dbaa9c7ebac9e99e63b977235b9919030b5
Jesse David Goerzen <jdgoerzen@gmail.com>
2024-01-11 19:26:23 +0000

    Fixed simplified string substitution

--- compile.sh
+++ compile.sh
@@ -10,8 +10,8 @@ if [ -f ./deploy.sh ]; then
 	sudo rm ./deploy.sh
 fi
 
-awk 'NR==FNR { a[n++]=$0 gsub(/\$/, "\\\$"); next } /REPLACEMEWITHDOCKERCOMPOSEFILE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./docker-compose.yaml ./deploy.sh.template | \
-	awk 'NR==FNR { a[n++]=$0 gsub(/\$/, "\\\$"); next } /REPLACEMEWITHDOTENVTEMPLATE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./.env.template - | \
-	awk 'NR==FNR { a[n++]=$0 gsub(/\$/, "\\\$"); next } /REPLACEMEWITHSERVICETEMPLATE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./docker-guacamole.service.template - > ./deploy.sh
+awk 'NR==FNR { gsub(/\$/, "\\\$"); a[n++]=$0; next } /REPLACEMEWITHDOCKERCOMPOSEFILE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./docker-compose.yaml ./deploy.sh.template | \
+	awk 'NR==FNR { gsub(/\$/, "\\\$"); a[n++]=$0; next } /REPLACEMEWITHDOTENVTEMPLATE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./.env.template - | \
+	awk 'NR==FNR { gsub(/\$/, "\\\$"); a[n++]=$0; next } /REPLACEMEWITHSERVICETEMPLATE/ { for (i=0;i<n;++i) print a[i]; next } 1' ./docker-guacamole.service.template - > ./deploy.sh
 
 sudo chmod 750 ./deploy.sh
